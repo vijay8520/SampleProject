627f7bb8a943e736336694737b97fd515b3ef256
Vijay <vijayg.provab@gmail.com>
2020-05-13 12:51:28 +0530

    Map feature

--- SampleProject/ViewController.swift
+++ SampleProject/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         
         print("First commit")
+        print("Map Feature")
+
     }
 
 
